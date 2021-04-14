4eed89a47f44d39eaec916ad1f62d490eddd7122
mohamed <mohamed.hamdy.issa@gmail.com>
2021-04-14 14:02:15 +0200

    building an api service with the dart http package

---
+++ lib/app/services/api_keys.dart
@@ -0,0 +1,4 @@
+class APIKeys {
+  static String ncovSandBoxKey =
+      "3dd214350c8c03593f0bfeced30a014415d6693fcb34183d43468c17113254e1";
+}
---
+++ lib/app/services/api_services.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+import 'package:coronavirus_rest_api/app/services/api.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+class APIServices {
+  final API api;
+
+  APIServices({@required this.api});
+
+  Future<String> getAccessToken() async {
+    final response = await http.post(api.tokenUri(),
+        headers: {'Authorization': 'Basic ${api.apiKey}'});
+    print(json.decode(response.body));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      print(response.body);
+      print(data['access_token']);
+      final accessToken = data['access_token'];
+      if (accessToken != null) {
+        return accessToken;
+      }
+    }
+    print(
+        'Request ${api.tokenUri()} failed\n Response: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
+  }
+
+  Future<int> getEndPointData(
+    // ignore: invalid_required_positional_param
+    @required String accessToken,
+    // ignore: invalid_required_positional_param
+    @required Endpoint endpoint,
+  ) async {
+    final url = api.endPointUrl(endpoint);
+    final response =
+        await http.get(url, headers: {'Authorization': 'Bearer $accessToken'});
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endPointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint];
+        final int result = endPointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+    print(
+        'Request $url failed\n Response: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
+  }
+
+  static Map<Endpoint, String> _responseJsonKeys = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesConfirmed: 'data',
+    Endpoint.casesSuspected: 'data',
+    Endpoint.deaths: 'data',
+    Endpoint.recovered: 'data',
+  };
+}
